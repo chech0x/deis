1e035afa6a80a535259ba624076355d49d6ac2ea
Sergio Campos <scampos@klabs.cl>
2016-06-09 15:26:13 -0400

    Se cambia constante de la version del cliente

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // Version identifies this Deis product revision.
-const Version = "1.14.0-dev"
+const Version = "0.0.2-dev"
 
 // APIVersion identifies the latest Deis api verison
 const APIVersion = "1.7"
